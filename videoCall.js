367eab7235820f3f6abd9801807a9b7cc90a0487
Brian <bleonar5@u.rochester.edu>
2022-05-22 08:29:55 -0400

    2nd

--- videoCall.js
+++ videoCall.js
@@ -1,6 +1,6 @@
 
   $('#joinButton').on('click',function(){
-  	async join();
+  	await join();
   });/*
  *  These procedures use Agora Video Call SDK for Web to enable local and remote
  *  users to join and leave a Video Call channel managed by Agora Platform.
