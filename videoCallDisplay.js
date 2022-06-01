14a4b96c99765b4aba1e839afc5f2cc0f967c50e
Brian <bleonar5@u.rochester.edu>
2022-06-01 07:23:20 -0400

    git

--- videoCallDisplay.js
+++ videoCallDisplay.js
@@ -261,6 +261,10 @@ function handleUserUnpublished(user, mediaType) {
     delete remoteUsers[id];
     console.log('TEST:');
     console.log(remoteUsers);
+    if(remoteUsers.keys.length == 0 and time_left != 0){
+    	time_left = 0;
+    	Qualtrics.SurveyEngine.setEmbeddedData("callCompleted", "false");
+    }
     $(`#player-wrapper-${id}`).remove();
 
   }
